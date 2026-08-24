6adb96c5e6e63aea280b8e93ad802e20d16ff051
Thiago Macedo <119901046+o-thiago@users.noreply.github.com>
2026-08-24 17:19:14 -0400

    feat(flake): add flake.nix for building CV PDFs and devShell

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "Thiago Macedo Mendes - Curriculum Vitae (LaTeX / ModernCV / Nix)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs@{ flake-parts, systems, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+      perSystem =
+        { pkgs, ... }:
+        let
+          tex = pkgs.texliveFull;
+
+          buildCv =
+            { name, srcDir }:
+            pkgs.stdenv.mkDerivation {
+              pname = "cv-${name}";
+              version = "1.0.0";
+              src = ./.;
+
+              nativeBuildInputs = [ tex ];
+
+              buildPhase = ''
+                cd resumes/${srcDir}
+                pdflatex -interaction=nonstopmode ${name}.tex
+                pdflatex -interaction=nonstopmode ${name}.tex
+              '';
+
+              installPhase = ''
+                mkdir -p $out
+                cp ${name}.pdf $out/
+              '';
+            };
+
+          cv-en = buildCv {
+            name = "resume";
+            srcDir = "en";
+          };
+          cv-pt = buildCv {
+            name = "curriculo";
+            srcDir = "pt-br";
+          };
+
+          cv-all = pkgs.stdenv.mkDerivation {
+            pname = "curriculum-vitae";
+            version = "1.0.0";
+            src = ./.;
+
+            nativeBuildInputs = [ tex ];
+
+            buildPhase = ''
+              mkdir -p build/en build/pt-br
+              cp -r resumes/en/* build/en/
+              (cd build/en && pdflatex -interaction=nonstopmode resume.tex)
+              cp -r resumes/pt-br/* build/pt-br/
+              (cd build/pt-br && pdflatex -interaction=nonstopmode curriculo.tex)
+            '';
+
+            installPhase = ''
+              mkdir -p $out/resumes/en $out/resumes/pt-br
+              cp -r resumes $out/
+              cp build/en/resume.pdf $out/resume.pdf
+              cp build/pt-br/curriculo.pdf $out/curriculo.pdf
+            '';
+          };
+        in
+        {
+          packages = {
+            default = cv-all;
+            en = cv-en;
+            pt = cv-pt;
+          };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ tex ];
+          };
+        };
+    };
+}
