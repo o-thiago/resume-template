4b1c87369bf26f4f9b4018b1ff6a651cf13998e7
Thiago Macedo <119901046+o-thiago@users.noreply.github.com>
2026-09-19 16:45:02 -0400

    feat(flake): use relative submodule git+file:./submodules/cv-data for cv-data

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
     cv-data = {
-      url = "git+file:///home/rika/Programming/cv-data";
+      url = "git+file:./submodules/cv-data";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-parts.follows = "flake-parts";
